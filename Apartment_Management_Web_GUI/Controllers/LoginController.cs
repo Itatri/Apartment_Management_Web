b5221da5a19695a18ee77475325a39271f24539c
Itatri <trihoangvan16092003@gmail.com>
2024-11-15 11:11:46 +0700

    Thêm đường dẫn chung cho ảnh chữ ký ở appsettings.Development.json và appsettings.Production.json

--- Apartment_Management_Web_GUI/Controllers/LoginController.cs
+++ Apartment_Management_Web_GUI/Controllers/LoginController.cs
@@ -5,23 +5,28 @@ namespace Apartment_Management_Web_GUI.Controllers
     public class LoginController : Controller
     {
         private readonly string _apiBaseUrl;
+        private readonly string _imageBaseUrl;
         // Khai bao APIUrl su dung chung 
         public LoginController(IConfiguration configuration)
         {
             _apiBaseUrl = configuration["ApiSettings:BaseUrl"];
+            _imageBaseUrl = configuration["ApiSettings:ImageBaseUrl"];
         }
 
         public IActionResult LoginPages()
         {
             // Truyền URL API vào View thông qua ViewBag
             ViewBag.ApiBaseUrl = _apiBaseUrl;
+            ViewBag.ImageBaseUrl = _imageBaseUrl;
             return View();
         }
 
         public IActionResult UserProfile()
         {
+
             // Truyền URL API vào View thông qua ViewBag
             ViewBag.ApiBaseUrl = _apiBaseUrl;
+            ViewBag.ImageBaseUrl = _imageBaseUrl;
             return View();
         }
 
